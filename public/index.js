e933cc17907e0a77ad9d5000317dee07c66815a0
Michael <mdj1999@gmail.com>
2019-06-08 12:11:29 -0700

    +Removes spaces -Removed comments

--- public/index.js
+++ public/index.js
@@ -1,19 +1,7 @@
 var search_box = document.querySelector('#nav_search_box');
 search_box.addEventListener('change', search);
 function search(event){
-   // searches = require('exampleQuiz.json');
-    //for(var i=0;i<searches.length;i++){
-     //   console.log(searches)
-       // if((searches[i].name.includes(search_box.value))||(searches[i].description.includes(search_box.value))){
-            //FIX ME//
-            var url='/search/'+search_box.value.toLowerCase();
-            console.log('===URL===', url);
-            //res=new XMLHttpRequest();
-            //res.open('GET', URL);
-            //res.send(body);
-            //return res.redirect(URL)
-            window.location.replace(url);
-        
-    
-
+    var url='/search/'+search_box.value.replace(/\s+/g, '').toLowerCase();
+    console.log('URL requested:', url);
+    window.location.replace(url);
 }
