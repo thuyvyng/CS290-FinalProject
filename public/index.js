a186a473adc44e554251a84f807b356a0663dbbf
MichaelJereza <35281729+MichaelJereza@users.noreply.github.com>
2019-06-09 13:28:59 -0700

    Delete index.js

--- public/index.js
+++
@@ -1,7 +0,0 @@
-var search_box = document.querySelector('#nav_search_box');
-search_box.addEventListener('change', search);
-function search(event){
-    var url='/search/'+search_box.value.replace(/\s+/g, '').toLowerCase();
-    console.log('URL requested:', url);
-    window.location.replace(url);
-}
