649cd8b090194f14bf43c8bca46d23bd90c0290f
ThuyVy <thuyvynguy@gmail.com>
2019-06-11 17:38:37 -0700

    Modal open and closes
    basically just opens and closes the modal

--- public/search.js
+++ public/search.js
@@ -6,4 +6,27 @@ function search(event){
     search_box.value='';
     window.location.assign(url);
 }
- 
+
+// For practice quiz
+
+var practice_button = document.getElementsByClassName('practice_button')[0];
+practice_button.addEventListener('click',showPracticeModal);
+
+function showPracticeModal(){
+  var practiceContainer = document.getElementsByClassName('practice_quiz_container')[0];
+  var practiceCard = document.getElementsByClassName('practice_card')[0];
+
+  practiceContainer.classList.remove('hidden');
+  practiceCard.classList.remove('hidden');
+}
+
+var close_practice_button = document.getElementsByClassName('modal_close_button')[0];
+close_practice_button.addEventListener('click',hidePracticeModal);
+
+function hidePracticeModal(){
+  var practiceContainer = document.getElementsByClassName('practice_quiz_container')[0];
+  var practiceCard = document.getElementsByClassName('practice_card')[0];
+
+  practiceContainer.classList.add('hidden');
+  practiceCard.classList.add('hidden');
+}
