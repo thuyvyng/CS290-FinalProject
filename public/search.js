5493177232647c1c661f99e6dd7c6b69274730e7
Michael <mdj1999@gmail.com>
2019-06-12 09:54:04 -0700

    +Won't search with only spaces

--- public/search.js
+++ public/search.js
@@ -1,9 +1,11 @@
 var search_box = document.querySelector('#nav_search_box');
 search_box.addEventListener('change', search);
 function search(event){
-    var url='/search/'+search_box.value.replace(/\s+/g, '+');
-    console.log('URL requested:', url);
-    search_box.value='';
-    window.location.assign(url);
+    if(search_box.value.replace(/\s+/g, '').length!=0){
+        var url='/search/'+search_box.value.replace(/\s+/g, '+');
+        console.log('URL requested:', url);
+        search_box.value='';
+        window.location.assign(url);
+    }
 }
  
