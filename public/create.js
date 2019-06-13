2b51098d8bd67c7d1af86143159369577ac4d51e
Toias <birdto@oregonstate.edu>
2019-06-13 08:59:01 -0700

    Add card works and delete card still works

--- public/create.js
+++ public/create.js
@@ -7,8 +7,8 @@ function add_card_code(){
 	answer: "Answer..."
     }
     var new_card_template = Handlebars.templates.newCard(newObject);
-    console.log("got template");
-    var cardContainer = document.queryselector('.complete_container');
+    console.log("got template ", new_card_template);
+    var cardContainer = document.querySelector('.complete_container');
     cardContainer.insertAdjacentHTML('beforeend', new_card_template);
 }
 
@@ -24,7 +24,7 @@ function delete_card_code(child){
 
 }
 
-function finish_card(){
+function finish_card(res){
     console.log('In finishing process');
     var title = document.getElementsByClassName('edit_quiz_title');
 	title = title[0].value;
@@ -39,18 +39,33 @@ function finish_card(){
 	var request = new XMLHttpRequest();
 	var requestURL = '/api/create';
 	request.open('POST', requestURL);
-	
+
+	var title_str = document.getElementsByClassName('edit_quiz_title');
+	title_str = title_str[0].value;
+	console.log('title: ', title_str);
+
+	var desc_str = document.getElementsByClassName('edit_quiz_description');
+	desc_str = desc_str[0].value;
+	console.log('description: ', desc_str);
+
+	var tagsArr = []; 
+	var tags_len = document.getElementsByClassName('edit_quiz_tags');
+	for (var i = 0; i < tags_len; i++){
+		var tags_val = tags_len[i].value;
+		tagsArr.push(tags_val);
+	}
+	console.log('tags: ', tagsArr);
+
 	var cardsArr = [[]];
-	var len = document.getElementsByClassName("edit_card_input");
-	for (var i = 0; i < len.length; i++){
+	var cards_len = document.getElementsByClassName("edit_card_input");
+	for (var i = 0; i < cards_len; i++){
 	    cardsArr
 	}
 	/*var quiz = {
-	    title:  
-	    description:
-	    tags: [
-
-	    ],
+	    id: 0, 
+	    title: title_str,
+	    description: desc_str,
+	    tags: tagsArr,
 	    cards: [
 
 	    ]
